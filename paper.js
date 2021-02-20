f3e95b5d2623ac5069e2650c13b62ebbea5afacd
vanshika0430 <72740634+vanshika0430@users.noreply.github.com>
2021-02-20 19:09:11 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,37 @@
+class Paper{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+
+
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+
+		this.image = loadImage("Paper.png");
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+
+			var pos=this.body.position;		
+
+			push()
+			translate(pos.x, pos.y);
+			imageMode(CENTER);
+			image(this.image,0,0,this.x,this.y);
+			//strokeWeight(4);
+         //   fill("purple");
+			ellipse(0,0,this.r, this.r);
+			pop()
+
+	}
+
+} 
